3e08e6d9ba4d3031296ce3e86ef1b4f6278b20de
stkhan2 <sadiakhan7545@gmail.com>
2025-11-20 16:40:50 -0500

    day 3

--- src/Grid.java
+++ src/Grid.java
@@ -1,6 +1,5 @@
 package src;
 
-public 
 public class Grid {
     
     public static int MAX_SCORE = 4;
@@ -11,31 +10,54 @@ public class Grid {
     private String[][] grid = new String[ROW][COLUMN];
 
     public Grid() {
-        for (int i = 0; i <= ROW; i++){
-            for(int j = 0; j <= COLUMN; j++){
+        for (int i = 0; i < ROW; i++){
+            for(int j = 0; j < COLUMN; j++){
                 grid[i][j] = " ";
             }
         }
     }
     
-    // public String getGrid(){
-    //     return fullGrid;
-    // }
+    public boolean isColumnFull(int column){
+        if (grid[7][column].equals("X") || grid[7][column].equals("O")) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
-    public String displayBoard() {
-        
+    public void placePiece(int column, String piece) {
+        if (!isColumnFull(column)) {
+            for (int i = 0; i < ROW; i++) {
+                if (grid[ROW][column].equals(" ")) {
+                    grid[ROW][column] = piece;
+                    break;
+                }
+            }
+        }
     }
 
     public String toString() {
         String fullGrid = "";
         for (int i = ROW-1; i >= 0; i--){
             for(int j = 1; j < COLUMN; j++){
-                fullGrid += grid[i][j];
+                fullGrid += "[" + grid[i][j] + "]";
             }
             fullGrid += "\n";
         }
         return fullGrid;
     }
+    
+    public static boolean isFull() {
+
+    }
+
+    public static void displayBoard() {
+    }
+
+    public static boolean validColumn(int columns){
+    }
+    
     
 }
 
--- test/gridTest.java
+++ test/gridTest.java
@@ -1,5 +1,7 @@
 package test;
 
 public class gridTest {
-    
+    Grid grid = new Grid();
+    System.out.println(grid.toString());
+
 }
